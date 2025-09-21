4d19beb9f66f8621ec40f30d74e939abc5344512
sangambhujbal <sangambhujbal12@gmail.com>
2025-09-21 17:50:45 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/config.php';
-SessionManager::start();
-Utils::logActivity('user_logout', ['user_id' => SessionManager::get('user_id')]);
-SessionManager::destroy();
-Response::redirect(SITE_URL . '/index.html');
